8011d5dbfe4b3e094e61c71831fc6a31fad4b17a
Irina <38900844+LLevella@users.noreply.github.com>
2019-03-15 17:00:54 +0700

    Add files via upload

---
+++ actor.js
@@ -0,0 +1,48 @@
+'use strict';
+class Actor{
+	constructor(position, size, speed){
+		if(position == undefined) position = new Vector(0,0);
+		if(size == undefined) size = new Vector(1,1);
+		if(speed == undefined) speed = new Vector(0,0);
+
+		if((position.constructor !== Vector) ||(size.constructor !== Vector)||(speed.constructor !== Vector))
+			throw "Ошибка приведения типов, переданный объект не является вектором типа Vector";
+		
+		this.pos = position;
+		this.size = size;
+		this.speed = speed;
+		
+	};
+
+	act(){};
+
+	get left(){
+		return this.pos.x;
+	};
+
+	get top(){
+		return this.pos.y;
+	};
+
+	get right(){
+		return this.pos.plus(this.size).x;
+	};
+
+	get bottom(){
+		return this.pos.plus(this.size).y;
+	};
+
+	get type(){
+		return this;
+	};
+
+	isIntersect(actor){
+		if(actor.constructor !== Actor)
+			throw "Ошибка приведения типов, переданный объект не является движущимся объектом типа Actor";
+		if(actor === this) return false;
+		if(((actor.left > this.left)&& (actor.left < this.right)||(this.left > actor.left)&& (this.left < actor.right)) &&
+			 ((actor.top > this.top)&& (actor.top < this.bottom)||(this.top > actor.top)&& (this.top < actor.bottom)))
+			 return true;
+		return false;
+	}
+}
---
+++ vector.js
@@ -0,0 +1,17 @@
+'use strict';
+class Vector{
+	constructor( x = 0, y = 0){
+		this.x = x;
+		this.y = y;
+	};
+	plus(v){
+		if(v.constructor !== Vector) 
+			throw "Ошибка приведения типов, переданный объект не является вектором типа Vector";
+		let sum = new Vector(this.x + v.x, this.y+ v.y);
+		return sum;
+	};
+	times(cft){
+		let v = new Vector(this.x * cft, this.y * cft);
+		return v;
+	};
+}
