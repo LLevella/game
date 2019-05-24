3f930f1e466a6f311bc3d7efc2df4bfcbd85565e
irina.khomutova <irina.khomutova@eltex.loc>
2019-05-24 15:34:10 +0700

    поправила форматирование

--- actor.js
+++ actor.js
@@ -1,47 +1,47 @@
 'use strict';
 class Actor{
-	constructor(position = new Vector(0,0), size = new Vector(1,1), speed = new Vector(0,0)){
+  constructor(position = new Vector(0,0), size = new Vector(1,1), speed = new Vector(0,0)){
 
-		if((position.constructor !== Vector) ||(size.constructor !== Vector)||(speed.constructor !== Vector))
-			throw Error("Ошибка приведения типов, переданный объект не является вектором типа Vector");
-		
-		this.pos = position;
-		this.size = size;
-		this.speed = speed;
-		
-	};
+    if((position.constructor !== Vector) ||(size.constructor !== Vector)||(speed.constructor !== Vector))
+      throw Error("Ошибка приведения типов, переданный объект не является вектором типа Vector");
+    
+    this.pos = position;
+    this.size = size;
+    this.speed = speed;
+    
+  };
 
-	act(){};
+  act(){};
 
-	get left(){
-		return this.pos.x;
-	};
+  get left(){
+    return this.pos.x;
+  };
 
-	get top(){
-		return this.pos.y;
-	};
+  get top(){
+    return this.pos.y;
+  };
 
-	get right(){
-		return this.pos.plus(this.size).x;
-	};
+  get right(){
+    return this.pos.plus(this.size).x;
+  };
 
-	get bottom(){
-		return this.pos.plus(this.size).y;
-	};
+  get bottom(){
+    return this.pos.plus(this.size).y;
+  };
 
-	get type(){
-		return 'actor';
-	};
+  get type(){
+    return 'actor';
+  };
 
-	isIntersect(actor){
-		if (!(actor instanceof Actor) || !actor) {
-			throw new Error(`В качестве аргумента можно передавать только вектор типа Vector`);
-		}
+  isIntersect(actor){
+    if (!(actor instanceof Actor) || !actor) {
+      throw new Error(`В качестве аргумента можно передавать только вектор типа Vector`);
+    }
 
-		if (actor === this) {
-			return false;
-		}
+    if (actor === this) {
+      return false;
+    }
 
-		return (this.left < actor.right && this.right > actor.left &&	this.bottom > actor.top &&	this.top < actor.bottom);
-	}
+    return (this.left < actor.right && this.right > actor.left &&  this.bottom > actor.top &&  this.top < actor.bottom);
+  }
 }
--- player.js
+++ player.js
@@ -1,10 +1,10 @@
 class Player extends Actor{
-	constructor(position){
-		super(position,new Vector(0.8, 1.5));
-		this.pos = this.pos.plus(new Vector(0, -0.5));
-	}
+  constructor(position){
+    super(position,new Vector(0.8, 1.5));
+    this.pos = this.pos.plus(new Vector(0, -0.5));
+  }
 
-	get type(){
-		return 'player';
-	}
+  get type(){
+    return 'player';
+  }
 }
