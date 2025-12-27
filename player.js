411d0ee0a97fde5c2a6378b458679a08a76be22e
Llevella <irinavkhomutova@gmail.com>
2025-12-27 12:08:19 +0300

    добавила проверку на наличие элемента сетки

--- player.js
+++ player.js
@@ -1,7 +1,9 @@
 class Player extends Actor{
   constructor(position){
-    super(position,new Vector(0.8, 1.5));
-    this.pos = this.pos.plus(new Vector(0, -0.5));
+		super(position, new Vector(0.8, 1.5));
+		console.log("Player", this.pos);
+		this.pos = this.pos.plus(new Vector(0, -0.5));
+		console.log("Player pos+", this.pos );
   }
 
   get type(){
